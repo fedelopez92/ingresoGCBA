ffcb1670ad6dcbb89c1666cfef66dc1ebad27417
Federico López <fedelopez1492@gmail.com>
2019-08-09 21:25:26 -0300

    Realizado diseño de la app (wireframe)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatosComponent } from './componentes/datos/datos.component';
+import { HistorialComponent } from './componentes/historial/historial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatosComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
